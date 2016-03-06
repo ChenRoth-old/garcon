95f8dca237c2a5083c9f24f46935fda9dbe51ea0
Chen Roth <chenr@gigaspaces.com>
2016-03-06 11:20:31 +0200

    add dependencies

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Garcon',
+      version='1.0',
+      description='become even lazier with cibus automated',
+      author='Chen Roth',
+      author_email='rothchen@gmail.com',
+      url='https://github.com/chenroth/garcon',
+      install_requires=[
+              'selenium',
+              'menu'])
